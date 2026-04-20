ebd3da6f917585f71b17891bb9ff0d368a18014d
bubbobne <daniele.andreis@gmail.com>
2026-04-20 14:20:02 +0200

    Import geoframe-core from temp

--- geoframe/geoframe-core/src/main/java/org/hortonmachine/geoframe/Test.java
+++ geoframe/geoframe-core/src/main/java/org/hortonmachine/geoframe/Test.java
@@ -105,7 +105,7 @@ public class Test extends HMModel {
 //		double northing = 5128704.4571;
 
 		// NOCE
-		String folder = "/home/hydrologis/development/hm_models_testdata/geoframe/newage/noce/";
+		String folder = "/home/andreisd/Documents/project/data_hm/vermiglio_dtm/";
 		String ext = ".tif";
 		int drainThres = 2000;
 		double desiredArea = 1_000_000.0;
@@ -134,12 +134,12 @@ public class Test extends HMModel {
 
 		String skyview = folder + "outputs/skyview" + ext;
 		String basinskyview = folder + "outputs/basin_skyview" + ext;
+		String geoframeGpkg = folder +"/outputs/geoframe_dataII.gpkg";
 
 		String geoframeFolder = folder + "outputs/geoframe_inputs/";
 
-		String geoframeGpkg = folder + "outputs/geoframe_data.gpkg";
 
-		String envDataPath = "/home/hydrologis/storage/lavori_tmp/GEOFRAME/env_data.sqlite";
+		String envDataPath = folder+"/outputs/env_data.sqlite";
 
 		File outFolder = new File(folder + "outputs/");
 		if (!outFolder.exists()) {
@@ -156,156 +156,6 @@ public class Test extends HMModel {
 
 		try {
 
-			if (toDo(pit)) {
-				OmsPitfiller pitfiller = new OmsPitfiller();
-				pitfiller.inElev = getRaster(dtm);
-				pitfiller.process();
-				dumpRaster(pitfiller.outPit, pit);
-				makeQgisStyleForRaster(EColorTables.elev.name(), pit, 0);
-			}
-
-			if (toDo(flow)) {
-				OmsFlowDirections flowdirections = new OmsFlowDirections();
-				flowdirections.inPit = getRaster(pit);
-				flowdirections.pMinElev = 0;
-				flowdirections.process();
-				dumpRaster(flowdirections.outFlow, flow);
-				makeQgisStyleForRaster(EColorTables.flow.name(), flow, 0);
-			}
-
-			if (toDo(drain)) {
-				OmsDrainDir draindir = new OmsDrainDir();
-				draindir.inPit = getRaster(pit);
-				draindir.inFlow = getRaster(flow);
-				draindir.pLambda = 1.0;
-				draindir.doLad = true;
-				draindir.process();
-				dumpRaster(draindir.outFlow, drain);
-				dumpRaster(draindir.outTca, tca);
-				makeQgisStyleForRaster(EColorTables.flow.name(), drain, 0);
-				makeQgisStyleForRaster(EColorTables.logarithmic.name(), tca, 0);
-			}
-
-			if (toDo(net)) {
-				OmsExtractNetwork extractnetwork = new OmsExtractNetwork();
-				extractnetwork.inTca = getRaster(tca);
-				extractnetwork.inFlow = getRaster(flow);
-				extractnetwork.pThres = drainThres;
-				extractnetwork.process();
-				dumpRaster(extractnetwork.outNet, net);
-				makeQgisStyleForRaster(EColorTables.net.name(), net, 0);
-			}
-
-			if (toDo(skyview)) {
-				OmsSkyview sv = new OmsSkyview();
-				sv.inElev = getRaster(pit);
-				sv.process();
-				dumpRaster(sv.outSky, skyview);
-				makeQgisStyleForRaster(EColorTables.slope.name(), skyview, 0);
-			}
-
-			if (toDo(basin)) {
-				OmsExtractBasin extractbasin = new OmsExtractBasin();
-				extractbasin.inFlow = getRaster(drain);
-				extractbasin.pEast = easting;
-				extractbasin.pNorth = northing;
-				extractbasin.process();
-				dumpRaster(extractbasin.outBasin, basin);
-				makeQgisStyleForRaster(EColorTables.net.name(), basin, 0);
-			}
-
-			if (toDo(basin_resized)) {
-				OmsRasterResizer resizer = new OmsRasterResizer();
-				GridCoverage2D basinGC = getRaster(basin);
-				resizer.inRaster = basinGC;
-				RegionMap dataRegionMap = HMRaster.fromGridCoverage(basinGC).getDataRegionMap();
-				Envelope dataEnvelope = dataRegionMap.toEnvelope();
-				Polygon dataPolygon = GeometryUtilities.createPolygonFromEnvelope(dataEnvelope);
-				SimpleFeatureCollection fc = FeatureUtilities
-						.featureCollectionFromGeometry(basinGC.getCoordinateReferenceSystem(), dataPolygon);
-				resizer.inVector = fc;
-				resizer.process();
-				dumpRaster(resizer.outRaster, basin_resized);
-			}
-
-			// cutout for the basin only
-			OmsCutOut cutout = new OmsCutOut();
-			if (toDo(basinpit)) {
-				cutout.inRaster = getRaster(pit);
-				cutout.inMask = getRaster(basin_resized);
-				cutout.process();
-				dumpRaster(cutout.outRaster, basinpit);
-				makeQgisStyleForRaster(EColorTables.elev.name(), basinpit, 0);
-			}
-
-			if (toDo(basindrain)) {
-				cutout = new OmsCutOut();
-				cutout.inRaster = getRaster(drain);
-				cutout.inMask = getRaster(basin_resized);
-				cutout.process();
-				dumpRaster(cutout.outRaster, basindrain);
-				makeQgisStyleForRaster(EColorTables.flow.name(), basindrain, 0);
-			}
-
-			if (toDo(basintca)) {
-				cutout = new OmsCutOut();
-				cutout.inRaster = getRaster(tca);
-				cutout.inMask = getRaster(basin_resized);
-				cutout.process();
-				dumpRaster(cutout.outRaster, basintca);
-				makeQgisStyleForRaster(EColorTables.logarithmic.name(), basintca, 0);
-			}
-
-			if (toDo(basinnet)) {
-				cutout = new OmsCutOut();
-				cutout.inRaster = getRaster(net);
-				cutout.inMask = getRaster(basin_resized);
-				cutout.process();
-				dumpRaster(cutout.outRaster, basinnet);
-				makeQgisStyleForRaster(EColorTables.net.name(), basinnet, 0);
-			}
-
-			if (toDo(basinskyview)) {
-				cutout = new OmsCutOut();
-				cutout.inRaster = getRaster(skyview);
-				cutout.inMask = getRaster(basin_resized);
-				cutout.process();
-				dumpRaster(cutout.outRaster, basinskyview);
-				makeQgisStyleForRaster(EColorTables.slope.name(), basinskyview, 0);
-			}
-
-			if (toDo(basinnetnum) || !db.hasTable(SqlName.m(GeoframeUtils.GEOFRAME_TOPOLOGY_TABLE))) {
-				OmsNetNumbering nn = new OmsNetNumbering();
-				nn.inFlow = getRaster(basindrain);
-				nn.inNet = getRaster(basinnet);
-				nn.inTca = getRaster(basintca);
-				nn.pDesiredArea = desiredArea;
-				nn.pDesiredAreaDelta = desiredAreaDelta;
-				nn.inGeoframeDb = db;
-				nn.process();
-				dumpRaster(nn.outNetnum, basinnetnum);
-				dumpRaster(nn.outBasins, basinnetbasins);
-				dumpRaster(nn.outDesiredBasins, basinnetbasinsdesired);
-//			FileUtilities.writeFile(nn.outGeoframeTopology, new File(topology));
-				makeQgisStyleForRaster(EColorTables.contrasting.name(), basinnetnum, 0);
-				makeQgisStyleForRaster(EColorTables.contrasting.name(), basinnetbasins, 0);
-				makeQgisStyleForRaster(EColorTables.contrasting.name(), basinnetbasinsdesired, 0);
-			}
-
-			if (!db.hasTable(SqlName.m(GeoframeUtils.GEOFRAME_BASIN_TABLE))
-					|| !db.hasTable(SqlName.m(GeoframeUtils.GEOFRAME_NETWORK_TABLE))) {
-				OmsGeoframeInputsBuilder builder = new OmsGeoframeInputsBuilder();
-				builder.inPitfiller = basinpit;
-				builder.inDrain = basindrain;
-				builder.inTca = basintca;
-				builder.inNet = basinnet;
-				builder.inSkyview = basinskyview;
-				builder.inBasins = basinnetbasinsdesired;
-				// builder.inGeoframeTopology = topology;
-				// builder.outFolder = geoframeFolder;
-				builder.inGeoframeDb = db;
-				builder.process();
-			}
 
 			// TODO here a potential evapotrans comes in
 
